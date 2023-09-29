0726d8b9398448300ea21a61b2fcb88db0e86aa4
muattarmehdi <muattermehdi@gmail.com>
2023-09-30 02:15:21 +0500

    Source code for blog website.

---
+++ javascript/script.js
@@ -0,0 +1,32 @@
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    const slides = document.getElementsByClassName("slide");
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {
+        slideIndex = 1;
+    }
+    slides[slideIndex - 1].style.display = "block";
+    setTimeout(showSlides, 3000); 
+}
+
+const form = document.querySelector('.contact-form form');
+
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    const name = document.getElementById('name').value.trim();
+    const email = document.getElementById('email').value.trim();
+    const message = document.getElementById('message').value.trim();
+
+    if (name === '' || email === '' || message === '') {
+        alert('Please fill out all fields.');
+    } else {
+        alert('Form submitted successfully!');
+        form.reset();
+    }
+});
